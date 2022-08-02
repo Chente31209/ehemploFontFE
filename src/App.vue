be211d545b4f24a04de1008de9b1e5ee7252b2fb
chente31209 <chente31209@hotmail.com>
2022-08-01 19:58:03 -0500

    desarollo del ejemplo

---
+++ src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div id="Formulario" class="container">
+      <div id="Nobre">
+        <div class="form-group">
+          <fieldset>
+            <label class="form-label mt-4" for="readOnlyInput">RFC</label>
+            <input class="form-control"  type="text"  v-model="rfc">
+          </fieldset>
+        </div>
+      </div>
+      <div id="PassWord">
+        <div class="form-group">
+          <fieldset>
+            <label class="form-label mt-4" for="readOnlyInput">Password</label>
+            <input class="form-control"  type="password" v-model="password">
+          </fieldset>
+        </div>
+      </div>
+      <div id=".key">
+        <div class="form-group">
+          <label for="formFile" class="form-label mt-4">sube el archivo .key</label>
+          <input class="form-control" type="file" accept=".key" id="key" >
+        </div>
+      </div>
+      <div id=".car">
+        <div class="form-group">
+          <label for="formFile" class="form-label mt-4">sube el archivo .cer</label>
+          <input class="form-control" type="file" accept=".cer" id="cer" >
+        </div>
+      </div>
+      <div id="SUMIT">
+      <button class="btn btn-primary" @click="btnEncriprar()"> Enviar </button>
+    </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data:function() {
+    return {
+      rfc:"",
+    password:"",
+    Eemail:"",
+    Epassword:"",
+    pkey:null,
+    pcer:null,
+    url : "http://trsffirmadigitalserviciocertificados-dev.eba-qrs52xqc.us-west-1.elasticbeanstalk.com/"
+    
+    }
+  },
+  methods:{
+    btnEncriprar(){
+      
+      this.$http.get(this.url+"Certificados/GetKeyToUploadCertificate?rfc="+this.rfc).then((response) => {
+        this.Epassword=this.$CryptoJS.AES.encrypt(this.password, response.data).toString()
+        this.pkey = document.getElementById("key").files[0]
+        this.pcer = document.getElementById("cer").files[0]
+        this.$http.post(this.url+"Certificados",{
+          RFC:this.rfc,
+          UserId:response.data,
+          Password:this.Epassword,
+          filecer:this.pcer,
+          filekey:this.pkey
+        }).then((res)=>{
+          console.log(res)
+        })
+      })
+      
+
+      
+      
+
+
+      alert(this.Eemail + "\n"+this.Epassword + "\n"+this.pkey+ "\n"+this.pcer)
+    }
+  }
+  
+}
+</script>
